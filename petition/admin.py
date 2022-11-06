ec5a92a578b126daea0d11fce3c2455ef6d16d7c
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-06 23:33:13 +0330

    Adding Petition model to admin panel

--- petition/admin.py
+++ petition/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from petition.models import Petition
 
-# Register your models here.
+
+class PetitionAdmin(admin.ModelAdmin):
+    list_display = ("title", "owner", "num_signatures", "goal")
+    search_fields = ("title", "description", "owner__username")
+    ordering = ("-created", "num_signatures", "goal")
+
+
+admin.site.register(Petition, PetitionAdmin)

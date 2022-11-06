a89d80989c8bd339c90536fce6a34a5f209ff7c2
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-06 23:35:27 +0330

    Implementing PetitionListCreateAPIView and PetitionRetrieveUpdateDestroyAPIView

--- petition/views.py
+++ petition/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+from petition.models import Petition
+from petition.paginations import DefaultPagination
+from petition.permissions import IsOwnerOrIsAdminOrReadOnly
+from petition.serializers import PetitionListCreateSerializer, PetitionRetrieveUpdateDestroySerializer
+
+
+class PetitionListCreateAPIView(ListCreateAPIView):
+    model = Petition
+    queryset = Petition.objects.all()
+    serializer_class = PetitionListCreateSerializer
+    permission_classes = [IsOwnerOrIsAdminOrReadOnly]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = {
+        "owner__username": ["exact"],
+    }
+    search_fields = ["title", "description", "owner__username"]
+    ordering_fields = ["created", "num_signatures"]
+    pagination_class = DefaultPagination
+
+
+class PetitionRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    model = Petition
+    queryset = Petition.objects.all()
+    serializer_class = PetitionRetrieveUpdateDestroySerializer
+    permission_classes = [IsOwnerOrIsAdminOrReadOnly]
+    lookup_field = "slug"

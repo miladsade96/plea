59f3a07005bc6be3a6696cd70b1678f8e28ffe13
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-06 21:17:59 +0330

    Setting static and media configuration

--- plea/settings.py
+++ plea/settings.py
@@ -125,6 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+STATICFILES_DIRS = [BASE_DIR / "statics"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

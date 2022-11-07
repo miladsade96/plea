40c30cc8612d16193e5dc7f24f683a8c42f0e60f
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-07 13:18:19 +0330

    Implementing SignatureListCreateAPIView

--- petition/views.py
+++ petition/views.py
@@ -1,13 +1,15 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 
-from petition.models import Petition
+from petition.models import Petition, Signature
 from petition.paginations import PetitionDefaultPagination
 from petition.permissions import IsOwnerOrIsAdminOrReadOnly
 from petition.serializers import (
     PetitionListCreateSerializer,
     PetitionRetrieveUpdateDestroySerializer,
+    SignatureListCreateSerializer,
 )
 
 
@@ -31,3 +33,20 @@ class PetitionRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = PetitionRetrieveUpdateDestroySerializer
     permission_classes = [IsOwnerOrIsAdminOrReadOnly]
     lookup_field = "slug"
+
+
+class SignatureListCreateAPIView(ListCreateAPIView):
+    model = Signature
+    queryset = Signature.objects.filter(is_anonymous=False, is_verified=True)
+    serializer_class = SignatureListCreateSerializer
+    permission_classes = [AllowAny]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = {
+        "petition__slug": ["icontains"],
+        "country": ["icontains"],
+        "city": ["icontains"]
+    }
+    search_fields = ["petition__slug", "email", "country", "city"]
+    ordering_fields = ["created"]
+    pagination_class = PetitionDefaultPagination
+    lookup_field = ["petition__slug", "country", "city", "email"]

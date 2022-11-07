0cb44ab82a1cad429408a34418b315ce08ae613e
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-07 19:50:08 +0330

    Refactoring using black

--- plea/settings.py
+++ plea/settings.py
@@ -146,9 +146,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
-    'DEFAULT_FILTER_BACKENDS': (
-        'django_filters.rest_framework.DjangoFilterBackend',
-    ),
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
 }
 
 

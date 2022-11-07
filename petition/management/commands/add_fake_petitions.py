3098fb0c6be96bff6bd1ff6462c2cd21d9bd56b0
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-07 10:39:01 +0330

    Implementing a command to generate fake petition objects

---
+++ petition/management/commands/add_fake_petitions.py
@@ -0,0 +1,36 @@
+from random import randint
+
+from django.contrib.auth.models import User
+from django.core.management import BaseCommand
+from faker import Faker
+
+from petition.models import Petition
+
+
+class Command(BaseCommand):
+    def __init__(self):
+        super(Command, self).__init__()
+        self.fake = Faker()
+
+    def handle(self, *args, **options):
+        for _ in range(10):
+            f_name = self.fake.first_name()
+            l_name = self.fake.last_name()
+            email = f"{f_name}.{l_name}@mail.com"
+            user = User.objects.create_user(
+                username=f"{f_name}_{l_name}".lower(),
+                email=email,
+                password="A@123456",
+                first_name=f_name,
+                last_name=l_name
+            )
+            user.save()
+
+            for _ in range(10):
+                petition = Petition.objects.create(
+                    title=self.fake.paragraph(nb_sentences=1),
+                    description=self.fake.paragraph(nb_sentences=100),
+                    owner=user,
+                    goal=randint(100, 1000000)
+                )
+                petition.save()

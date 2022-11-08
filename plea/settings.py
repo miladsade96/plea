d8236eaf8c1f518ddea570e8a45ecdec274f81cc
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-08 19:07:56 +0330

    Adding celery and redis configuration

--- plea/settings.py
+++ plea/settings.py
@@ -157,3 +157,22 @@ EMAIL_HOST = "smtp4dev"
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 25
+
+
+# Celery configurations
+CELERY_BROKER_URL = "redis://plea_redis:6379/1"  # pattern: redis://redis_ip_or_container_name:port/database
+# celery start command inside docker compose --> celery -A core worker -l INFO
+# celery beat start command inside backend service -->
+# celery -A core  beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
+
+
+# Redis caching configurations
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://plea_redis:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}

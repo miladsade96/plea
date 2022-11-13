5fc250e1238884c1e1f8d3ccea9e98af695a99b8
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-13 18:43:01 +0330

    Adding custom user model to admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from accounts.models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "is_superuser", "is_active")
+    list_filter = ("is_superuser", "is_active")
+    search_fields = ("email", "username")
+    ordering = ("-created_at",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)

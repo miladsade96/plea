70459f7e7d55fbc371d485488867bbd533c11cb8
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-13 18:48:35 +0330

    making tasks parameters more dynamic

--- petition/tasks.py
+++ petition/tasks.py
@@ -3,11 +3,11 @@ from mail_templated import EmailMessage
 
 
 @shared_task
-def send_signature_verification_email(token, email, full_name):
+def send_signature_verification_email(token, sender, receiver, full_name):
     email = EmailMessage(
         template_name="email/signature_verification.tpl",
-        from_email="no_reply@plea.org",
-        to=[email],
+        from_email=sender,
+        to=[receiver],
         context={
             "token": token,
             "full_name": full_name,

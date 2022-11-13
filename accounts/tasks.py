d9ef7d4ba4c6b1a98063039946ce7a94e2329853
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-13 18:42:35 +0330

    Creating tasks.py and implementing account activation task

---
+++ accounts/tasks.py
@@ -0,0 +1,16 @@
+from celery import shared_task
+from mail_templated import EmailMessage
+
+
+@shared_task
+def send_account_activation_email(token, sender, receiver, full_name):
+    email = EmailMessage(
+        template_name="email/account_activation.tpl",
+        from_email=sender,
+        to=[receiver],
+        context={
+            "token": token,
+            "full_name": full_name,
+        },
+    )
+    email.send()

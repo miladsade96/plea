f2287637179b4b3cfd0ebdfe39c7cf2c9f0bab0c
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-13 23:30:05 +0330

    Fixing -Unable to delete user with corresponding outstanding token-

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from accounts.models import CustomUser
+from rest_framework_simplejwt.token_blacklist.admin import OutstandingTokenAdmin
+from rest_framework_simplejwt.token_blacklist.models import OutstandingToken
 
 
 class CustomUserAdmin(admin.ModelAdmin):
@@ -10,4 +12,11 @@ class CustomUserAdmin(admin.ModelAdmin):
     ordering = ("-created_at",)
 
 
+class CustomOutstandingTokenAdmin(OutstandingTokenAdmin):
+    def has_delete_permission(self, *args, **kwargs):
+        return True
+
+
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.unregister(OutstandingToken)
+admin.site.register(OutstandingToken, CustomOutstandingTokenAdmin)

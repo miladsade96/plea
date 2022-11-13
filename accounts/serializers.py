b1a6853d465e352fc2698ab14459819b5cb317ef
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-13 19:55:51 +0330

    Implementing ChangeUserPassword serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -31,3 +31,24 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop("confirm_password", None)
         return CustomUser.objects.create_user(**validated_data)
+
+
+class ChangeUserPasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(max_length=100, required=True)
+    new_password = serializers.CharField(max_length=100, required=True)
+    new_password_confirm = serializers.CharField(max_length=100, required=True)
+
+    def create(self, validated_data):
+        return super(ChangeUserPasswordSerializer, self).create(validated_data)
+
+    def update(self, instance, validated_data):
+        return super(ChangeUserPasswordSerializer, self).update(validated_data)
+
+    def validate(self, attrs):
+        if attrs.get("new_password") != attrs.get("new_password_confirm"):
+            raise serializers.ValidationError({"detail": "Password does not match!"})
+        try:
+            validate_password(attrs.get("new_password"))
+        except exceptions.ValidationError as errors:
+            raise serializers.ValidationError({"detail": list(errors.messages)})
+        return super(ChangeUserPasswordSerializer, self).validate(attrs)

ecb8892a6c0b65b0b58c2048f6b9b617bde79772
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-13 18:14:04 +0330

    Implementing CustomUser model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-created_at",)
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+
+    def __str__(self) -> str:
+        return self.username

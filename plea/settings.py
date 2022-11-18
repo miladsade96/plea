1b0a853f5b344726d417fdaf6eae8517f65e50b5
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-18 13:11:19 +0330

    Specifying login_url, login_redirect_url and logout_redirect_url

--- plea/settings.py
+++ plea/settings.py
@@ -93,6 +93,9 @@ DATABASES = {
 
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+LOGIN_URL = "/api-auth/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators

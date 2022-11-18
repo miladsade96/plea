30246932be7b4745e03aac1735f8ddbb135eb87d
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-18 20:22:43 +0330

    Implementing clean_up_inactive_user_account task

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,5 +1,10 @@
+from datetime import timedelta
+
 from celery import shared_task
+from django.utils import timezone
 from mail_templated import EmailMessage
+
+from accounts.models import CustomUser
 
 
 @shared_task
@@ -28,3 +33,10 @@ def send_password_reset_email(token, sender, receiver, full_name):
         },
     )
     email.send()
+
+
+@shared_task
+def clean_up_inactive_user_accounts():
+    CustomUser.objects.filter(
+        is_active=False, created_at__gt=timezone.now() - timedelta(days=7)
+    ).delete()

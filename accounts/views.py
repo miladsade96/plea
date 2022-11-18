c50c23b12d61ca9a3dec43f897019e78e6547787
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-18 20:23:40 +0330

    Refactoring by black

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,12 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from jwt import ExpiredSignatureError, InvalidSignatureError
 from rest_framework import status
-from rest_framework.generics import CreateAPIView, UpdateAPIView, GenericAPIView, DestroyAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    UpdateAPIView,
+    GenericAPIView,
+    DestroyAPIView,
+)
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView

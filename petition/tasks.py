46e92999ee12c876c79357298fbcb5becfd147c1
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-20 13:47:04 +0330

    Implementing send_successful_petition_report task

--- petition/tasks.py
+++ petition/tasks.py
@@ -14,3 +14,18 @@ def send_signature_verification_email(token, sender, receiver, full_name):
         },
     )
     email.send()
+
+
+@shared_task
+def send_successful_petition_report(title, owner, recipient_name, recipient_email):
+    email = EmailMessage(
+        template_name="email/successful_petition_report.tpl",
+        from_email="petition_report@plea.org",
+        to=[recipient_email],
+        context={
+            "title": title,
+            "owner": owner,
+            "recipient_name": recipient_name
+        },
+    )
+    email.send()

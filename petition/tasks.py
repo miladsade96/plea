6c725dd28ae1d4aa8029ab91ea9363230d7fab22
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2022-11-24 15:29:56 -0500

    Using csv instead of pdf as a report file

--- petition/tasks.py
+++ petition/tasks.py
@@ -4,9 +4,7 @@ from celery import shared_task
 from mail_templated import EmailMessage
 
 import io
-from reportlab.pdfgen import canvas
-from reportlab.lib.units import cm
-from reportlab.lib.pagesizes import letter
+import csv
 
 
 @shared_task
@@ -23,20 +21,14 @@ def send_signature_verification_email(token, sender, receiver, full_name):
     email.send()
 
 
-def pdf_generator(data):
-    buffer = io.BytesIO()
-    c = canvas.Canvas(buffer, pagesize=letter, bottomup=0)
-    text_obj = c.beginText()
-    text_obj.setTextOrigin(cm, cm)
-    text_obj.setFont("Helvetica", 12)
-    desc = wrap(data.get("petition_description"), width=40)
-    text_obj.textLines(desc)
-    c.drawText(text_obj)
-    c.showPage()
-    c.save()
-    pdf = buffer.getvalue()
-    buffer.close()
-    return pdf
+def csv_generator(data):
+    output_file = io.StringIO()
+    cr = csv.writer(output_file, delimiter=",", qouting=csv.QUOTE_ALL)
+    cr.writerow(["First_name", "Last_name", "email", "Country"])
+    for sgn in data:
+        cr.writerow([sgn[0], sgn[1], sgn[2], sgn[3]])
+    output_file.getvalue()
+    return output_file
 
 
 @shared_task
